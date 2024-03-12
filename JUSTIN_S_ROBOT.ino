1d6854b9f6bc7db8172b580145b08e5b0be53fa3
justin01 <justincalvin01@gmail.com>
2024-03-12 19:58:22 -0400

    Initial commit

---
+++ JUSTIN_S_ROBOT.ino
@@ -0,0 +1,18 @@
+int led = 13;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led,HIGH);
+  delay(250);
+  digitalWrite(led,LOW);
+  delay(250);
+}
